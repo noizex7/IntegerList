a5d299febbd149b57020e1a1db33f6f0bda3e961
noizex7 <61851047+noizex7@users.noreply.github.com>
2020-03-24 20:43:55 -0400

    ahora si

--- IntegerList/Source.cpp
+++ IntegerList/Source.cpp
@@ -7,8 +7,6 @@ int main()
 
     cout << "Enter he quantity of elements: ";
     cin >> n;
-    // Store 9s in the list and display an asterisk 
-    // each time a 9 is successfully stored. 
     for (x = 0; x < n; x++)
     {
         cout << "Value #" << x << ":";
@@ -22,15 +20,10 @@ int main()
 
     for (x = 0; x < n; x++)
     {
-        //val = numbers.getElement(x);
         cout << numbers.getElement(x) << " ";
     }
     cout << endl;
     numbers.displayLowestandHighest(n);
-    // Attempt to store a value outside the list's bounds. 
-    /* numbers.setElement(50, 9); */
-
-        // Will this message display? 
-       /* cout << "Element 50 successfully set.\n";*/
+    
     return 0;
 }
